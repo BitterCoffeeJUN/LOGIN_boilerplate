c03e036fb8443c46f0b6b1118a37ffe986e77655
BitterCoffeeJun <gurwns232@naver.com>
2021-09-23 03:21:07 +0900

    -client redux base code

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './views/App'
 
+import {BrowserRouter} from 'react-router-dom'
+
+import {Provider} from 'react-redux'
+import {createStore, applyMiddleware} from 'redux'
+import Reducer from './_reducers/index'
+import ReduxThunk from 'redux-thunk'
+import promiseMiddleware from 'redux-promise'
+
+const store = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
+
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+    <Provider
+        store={store(
+            Reducer,
+            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+        )}
+    >
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>,
     document.getElementById('root'),
 )
