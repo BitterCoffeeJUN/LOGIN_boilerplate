3513522c54fa05e49acccd6b3d91bf48aea8c592
BitterCoffeeJun <gurwns232@naver.com>
2021-09-23 02:58:00 +0900

    -client LoginPage 기본 틀

--- client/src/views/components/LoginPage/LoginPage.js
+++ client/src/views/components/LoginPage/LoginPage.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function LoginPage() {
+    const [Email, setEmail] = useState('')
+    const [Password, setPassword] = useState('')
+
+    const onEmailHandler = e => {
+        setEmail(e.currentTarget.value)
+    }
+    const onPasswordHandler = e => {
+        setPassword(e.currentTarget.value)
+    }
+
+    const onSubmitHandler = () => {}
+
     return (
         <div>
-            <h1>LoginPage</h1>
+            <form
+                style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    height: '100vh',
+                }}
+                onSubmit={onSubmitHandler}
+            >
+                <label htmlFor="">Email</label>
+                <input type="text" placeholder="email" value={Email} onChange={onEmailHandler} />
+                <label htmlFor="">Password</label>
+                <input
+                    type="password"
+                    placeholder="password"
+                    value={Password}
+                    onChange={onPasswordHandler}
+                />
+
+                <button>login</button>
+            </form>
         </div>
     )
 }
